ffc51fcda41758005284988f51ffe7ea1ff34f4d
Thomas Versteeg <thomasversteeg@gmx.com>
2016-02-07 16:19:31 +0700

    Add bootstrap options

--- batterycheck.sh
+++ batterycheck.sh
@@ -4,5 +4,5 @@ battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 if [ $battery_level -le 50 ]
 then
 	paplay /usr/share/sounds/purple/receive.wav ;
-	export DISPLAY=:0.0 && notify-send -i /usr/share/icons/Adwaita/48x48/status/battery-caution.png -u critical "Battery low" "Battery level is ${battery_level}%!"
+	export DISPLAY=:0.0 && notify-send -i /usr/share/icons/Adwaita/48x48/status/battery-caution.png "Battery low" "Battery level is ${battery_level}%!"
 fi
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,45 +9,69 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-### CUSTOM SCRIPTS ###
-
-# Add battery notification
-mkdir -p ~/.local/etc
-cp $DIR/batterycheck.sh ~/.local/etc
-chmod 777 ~/.local/etc
-
-# Add scripts to cron
-TEMP=$(mktemp)
-echo "* * * * * \"/home/$(users)/.local/etc/batterycheck.sh\"" > ${TEMP}
-crontab ${TEMP}
-rm -f ${TEMP}
-
-exit 0
-
 ### APPLICATIONS ###
-sudo apt-get -y install iceweasel
-sudo sed -i 's/^Exec.*/Exec=env GTK2_RC_FILES=\/usr\/share\/themes\/Adwaita\/gtk-2.0\/gtkrc iceweasel %u/' /usr/share/applications/iceweasel.desktop
+read -p "Install core packages? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get -y install iceweasel
+fi
 
 # Link the dotfiles
-cd ~
-ln -s $DIR/.*
+read -p "Create symlinks for the dotfiles? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	ls ls -l $DIR/.[^.]* | less
+	cd ~
+	ln -s $DIR/.*
+fi
+
+### CUSTOM SCRIPTS ###
+read -p "Add a battery notification to cron? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	# Add battery notification
+	mkdir -p ~/.local/etc
+	cp $DIR/batterycheck.sh ~/.local/etc
+	chmod 777 ~/.local/etc
+
+	# Add scripts to cron
+	TEMP=$(mktemp)
+	echo "* * * * * \"/home/$(users)/.local/etc/batterycheck.sh\"" > ${TEMP}
+	crontab ${TEMP}
+	rm -f ${TEMP}
+fi
+
+read -p "Apply the Adwaita theme to iceweasel? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo sed -i 's/^Exec.*/Exec=env GTK2_RC_FILES=\/usr\/share\/themes\/Adwaita\/gtk-2.0\/gtkrc iceweasel %u/' /usr/share/applications/iceweasel.desktop
+fi
 
 ### CUSTOM FONT ###
-# Install the tamsyn font
-cd /tmp
-wget http://www.fial.com/~scott/tamsyn-font/download/tamsyn-font-1.11.tar.gz
+read -p "Download & install the tamsyn font? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	# Install the tamsyn font
+	cd /tmp
+	wget http://www.fial.com/~scott/tamsyn-font/download/tamsyn-font-1.11.tar.gz
 
-mkdir /tmp/tamsyn
-cd /tmp/tamsyn
-tar -zxvf /tmp/tamsyn-font-1.11.tar.gz
+	mkdir /tmp/tamsyn
+	cd /tmp/tamsyn
+	tar -zxvf /tmp/tamsyn-font-1.11.tar.gz
 
-sudo mkdir -p /usr/local/share/fonts/bitmap
-sudo chmod 777 /usr/local/share/fonts/bitmap
-cd /usr/local/share/fonts/bitmap
-mv /tmp/tamsyn/* .
+	sudo mkdir -p /usr/local/share/fonts/bitmap
+	sudo chmod 777 /usr/local/share/fonts/bitmap
+	cd /usr/local/share/fonts/bitmap
+	mv /tmp/tamsyn/* .
 
-fc-cache -fv | grep -i "tamsyn"
-mkfontscale
-mkfontdir
-sudo dpkg-reconfigure fontconfig
-sudo dpkg-reconfigure fontconfig-config
+	fc-cache -fv | grep -i "tamsyn"
+	mkfontscale
+	mkfontdir
+	sudo dpkg-reconfigure fontconfig
+	sudo dpkg-reconfigure fontconfig-config
+fi
