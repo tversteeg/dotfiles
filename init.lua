12a52311f73ba9ee9a3ca661511e2e3890ecb0a6
Thomas Versteeg <thomas@versteeg.email>
2021-02-08 10:08:48 +0100

    Add lazyloading to vim plugins

--- init.lua
+++ init.lua
@@ -1,16 +1,13 @@
 --[[ Bootstrap Package Manager ]]
 do
-	local cmd = vim.cmd
-	local fn = vim.fn
-
 	-- Clone the git repository if not found
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
+	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+		vim.cmd("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
 	end
 
 	-- Initialize the package manager
-	cmd("packadd packer.nvim")
+	vim.cmd("packadd packer.nvim")
 end
 
 --[[ Packages ]]
@@ -19,55 +16,62 @@ require("packer").startup(function()
 	use {"wbthomason/packer.nvim", opt = true}
 
 	-- Purple color theme
-	use "yassinebridi/vim-purpura"
-	do
-		vim.cmd("colorscheme purpura")
-	end
+	use {
+		"yassinebridi/vim-purpura",
+		config = function()
+			vim.cmd("colorscheme purpura")
+		end,
+	}
 
 	-- Language Server
-	use "neovim/nvim-lspconfig"
-	-- Language Server extensions, type inlay hints
-	use "nvim-lua/lsp_extensions.nvim"
-	-- Language Server completions
-	use "nvim-lua/completion-nvim"
-	do
-		local cmd = vim.cmd
-		local map = vim.api.nvim_set_keymap
-		local lsp = require "lspconfig"
+	use {
+		"neovim/nvim-lspconfig",
+		requires = {
+			-- Language Server extensions, type inlay hints
+			"nvim-lua/lsp_extensions.nvim",
+			-- Language Server completions
+			"nvim-lua/completion-nvim",
+		},
+		ft = "rust",
+		config = function()
+			local lsp = require "lspconfig"
 
-		-- Attach the plugins to the LSP
-		local on_attach = function(client)
-			local completion = require "completion"
+			-- Attach the plugins to the LSP
+			local on_attach = function(client)
+				local completion = require "completion"
 
-			completion.on_attach(client)
+				completion.on_attach(client)
+			end
+
+			-- Setup rust-analyzer
+			lsp.rust_analyzer.setup({on_attach = on_attach})
+
+			-- Map the shortcuts
+			local function lsp_map(shortcut, name)
+				vim.api.nvim_set_keymap("n", shortcut, "<cmd>lua vim.lsp.buf." .. name .. "()<CR>", {noremap = true, silent = true})
+			end
+			lsp_map("gd", "declaration")
+			lsp_map("<c-]>", "definition")
+			lsp_map("K", "hover")
+			lsp_map("gD", "implementation")
+			lsp_map("<c-k>", "signature_help")
+			lsp_map("1gD", "type_definition")
+			lsp_map("gr", "references")
+			lsp_map("g0", "document_symbol")
+			lsp_map("ga", "code_action")
+
+			-- Enable type inlay hints
+			vim.cmd("autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require\"lsp_extensions\".inlay_hints{prefix = \"\", highlight = \"NonText\"}")
 		end
-
-		-- Setup rust-analyzer
-		lsp.rust_analyzer.setup({on_attach = on_attach})
-
-		-- Map the shortcuts
-		local function lsp_map(shortcut, name)
-			map("n", shortcut, "<cmd>lua vim.lsp.buf." .. name .. "()<CR>", {noremap = true, silent = true})
-		end
-		lsp_map("gd", "declaration")
-		lsp_map("<c-]>", "definition")
-		lsp_map("K", "hover")
-		lsp_map("gD", "implementation")
-		lsp_map("<c-k>", "signature_help")
-		lsp_map("1gD", "type_definition")
-		lsp_map("gr", "references")
-		lsp_map("g0", "document_symbol")
-		lsp_map("ga", "code_action")
-
-		-- Enable type inlay hints
-		cmd("autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost * lua require\"lsp_extensions\".inlay_hints{prefix = \"\", highlight = \"NonText\"}")
-	end
+	}
 
 	-- Show git blame info on the current line
-	use "APZelos/blamer.nvim"
-	do
-		vim.g.blamer_enabled = 1
-	end
+	use {
+		"APZelos/blamer.nvim",
+		config = function()
+			vim.g.blamer_enabled = 1
+		end,
+	}
 
 	-- Switch between relative and absolute numbers
 	use "jeffkreeftmeijer/vim-numbertoggle"
@@ -76,77 +80,106 @@ require("packer").startup(function()
 	use "ap/vim-buftabline"
 
 	-- Show and remove extra whitespace
-	use "ntpeters/vim-better-whitespace"
-	do
-		-- Strip all whitespace on save, disable with :DisableStripWhitespaceOnSave
-		vim.g.strip_whitespace_on_save = 1
-	end
+	use {
+		"ntpeters/vim-better-whitespace",
+		config = function()
+			-- Strip all whitespace on save, disable with :DisableStripWhitespaceOnSave
+			vim.g.strip_whitespace_on_save = 1
+		end,
+	}
 
 	-- Highlight f & t symbols
-	use "unblevable/quick-scope"
-	do
-		-- Trigger a highlight in the appropriate direction when pressing these keys
-		vim.g.qs_highlight_on_keys = {"f", "F", "t", "T"}
-	end
+	use {
+		"unblevable/quick-scope",
+		config = function()
+			-- Trigger a highlight in the appropriate direction when pressing these keys
+			vim.g.qs_highlight_on_keys = {"f", "F", "t", "T"}
+		end,
+	}
 
+	-- Set the terminal colors needed for the colorizer
+	vim.o.termguicolors = true
 	-- Highlight color text
-	use "norcalli/nvim-colorizer.lua"
-	do
-		local colorizer = require "colorizer"
+	use {
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			local colorizer = require "colorizer"
 
-		colorizer.setup()
-	end
+			colorizer.setup()
+		end,
+	}
 
 	-- Add rainbow parentheses
-	use "luachen1990/rainbow"
-	do
-		vim.g.rainbow_active = 1
-	end
+	use {
+		"luachen1990/rainbow",
+		config = function()
+			vim.g.rainbow_active = 1
+		end,
+	}
 
 	-- Code snippets
-	use "SirVer/ultisnips"
-	do
-		vim.g.UltiSnipsExpandTrigger = "<c-s>"
-		vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
-		vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
-		-- Split the window
-		vim.g.UltiSnipsEditSplit = "vertical"
-	end
+	use {
+		"SirVer/ultisnips",
+		config = function()
+			vim.g.UltiSnipsExpandTrigger = "<c-s>"
+			vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
+			vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
+			-- Split the window
+			vim.g.UltiSnipsEditSplit = "vertical"
+		end,
+	}
 
 	-- Fuzzy find
-	use {"junegunn/fzf.vim", requires = {"junegunn/fzf"}}
-	do
-		local map = vim.api.nvim_set_keymap
+	use {
+		"junegunn/fzf.vim",
+		requires = {"junegunn/fzf"},
+		config = function()
+			-- Map shortcuts
+			vim.api.nvim_set_keymap("", "<C-p>", ":GFiles<CR>", {})
+			vim.api.nvim_set_keymap("!", "<C-p>", ":GFiles<CR>", {})
+			vim.api.nvim_set_keymap("", "<C-.>", ":FZF<CR>", {})
+			vim.api.nvim_set_keymap("!", "<C-.>", ":FZF<CR>", {})
 
-		-- Map shortcuts
-		map("", "<C-p>", ":GFiles<CR>", {})
-		map("!", "<C-p>", ":GFiles<CR>", {})
-		map("", "<C-.>", ":FZF<CR>", {})
-		map("!", "<C-.>", ":FZF<CR>", {})
-
-		-- Use bat for FZF previews
-		vim.g.fzf_files_options = "--preview \"bat {}\""
-	end
+			-- Use bat for FZF previews
+			vim.g.fzf_files_options = "--preview \"bat {}\""
+		end,
+	}
 
 	-- Rust
-	use "rust-lang/rust.vim"
-	-- Rust crate versions
-	use "mhinz/vim-crates"
-	do
-		local cmd = vim.cmd
+	use {
+		"rust-lang/rust.vim",
+		config = function()
+			-- Autoformat Rust on save
+			vim.g.rustfmt_autosave = 1
+		end,
+	}
 
-		-- Autoformat Rust on save
-		vim.g.rustfmt_autosave = 1
+	-- Luacheck
+	use {
+		"vim-syntastic/syntastic",
+		ft = "lua",
+		config = function()
+			vim.g.syntastic_lua_checkers = {"luacheck"}
+		end,
+	}
 
-		-- Show outdated crates in Cargo.toml
-		cmd("autocmd BufRead Cargo.toml call crates#toggle()")
-	end
+	-- TOML
+	use {
+		"cespare/vim-toml",
+		-- Rust crate versions
+		requires = {"mhinz/vim-crates"},
+		ft = "toml",
+		config = function()
+			-- Show outdated crates in Cargo.toml
+			vim.cmd("autocmd BufRead Cargo.toml call crates#toggle()")
+		end,
+	}
 
-	-- TOML syntax
-	use "cespare/vim-toml"
-
-	-- RON syntax
-	use "ron-rs/ron.vim"
+	-- RON
+	use {
+		"ron-rs/ron.vim",
+		ft = "ron",
+	}
 
 	-- Keep track of the time spent programming with wakatime
 	use "wakatime/vim-wakatime"
@@ -154,76 +187,65 @@ end)
 
 --[[ Global Options ]]
 do
-	local o = vim.o
-
 	-- Always use UTF-8
-	o.encoding = "utf-8"
+	vim.o.encoding = "utf-8"
 
 	-- Automatically reload files changed outside of vim
-	o.autoread = true
+	vim.o.autoread = true
 
 	-- Don't wait 4 seconds for a popup to show
-	o.updatetime = 1000
+	vim.o.updatetime = 1000
 
 	-- Set the title in the terminal
-	o.title = true
+	vim.o.title = true
 
 	-- Where to store the undo files
-	o.undodir = "~/.config/nvim/undo"
+	vim.o.undodir = "~/.config/nvim/undo"
 
 	-- Show live preview of substitutions
-	o.inccommand = "split"
+	vim.o.inccommand = "split"
 
 	-- Set the minimal width of the window
-	o.winwidth = 80
-
-	-- Set the terminal colors needed for the colorizer
-	o.termguicolors = true
+	vim.o.winwidth = 80
 end
 
 --[[ Window Options ]]
 do
-	local wo = vim.wo
-
 	-- Display the line numbers sidebar
-	wo.number = true
-	wo.relativenumber = true
+	vim.wo.number = true
+	vim.wo.relativenumber = true
 
 	-- Highlight the current line
-	wo.cursorline = true
-	wo.cursorcolumn = true
+	vim.wo.cursorline = true
+	vim.wo.cursorcolumn = true
 
 	-- Automatically hide some symbols (mainly markdown)
-	wo.conceallevel = 2
+	vim.wo.conceallevel = 2
 
 	-- Always show the sign column
-	wo.signcolumn = "yes"
+	vim.wo.signcolumn = "yes"
 end
 
 --[[ Buffer Options ]]
 do
-	local bo = vim.bo
-
 	-- Save undo across all sessions
-	bo.undofile = true
+	vim.bo.undofile = true
 
 	-- Don't use swap files
-	bo.swapfile = false
+	vim.bo.swapfile = false
 end
 
 --[[ Auto Commands ]]
 do
-	local cmd = vim.cmd
-
 	-- Create an autocommand group
 	-- TODO: replace with https://github.com/neovim/neovim/pull/12378
 	local function create_augroup(autocmds, name)
-		cmd("augroup " .. name)
-		cmd("autocmd!")
+		vim.cmd("augroup " .. name)
+		vim.cmd("autocmd!")
 		for _, autocmd in ipairs(autocmds) do
-			cmd("autocmd " .. autocmd)
+			vim.cmd("autocmd " .. autocmd)
 		end
-		cmd("augroup END")
+		vim.cmd("augroup END")
 	end
 
 	-- Lua indentation
@@ -245,15 +267,13 @@ end
 
 --[[ Key Maps ]]
 do
-	local map = vim.api.nvim_set_keymap
-
 	-- Use jj instead of <esc>
-	map("i", "jj", "<esc>", {noremap = true})
-	map("i", "<esc>", "", {noremap = true})
+	vim.api.nvim_set_keymap("i", "jj", "<esc>", {noremap = true})
+	vim.api.nvim_set_keymap("i", "<esc>", "", {noremap = true})
 
 	-- Move across wrapped lines like regular lines
 	-- Go to the first non-blank character of a line
-	map("n", "0", "^", {noremap = true})
+	vim.api.nvim_set_keymap("n", "0", "^", {noremap = true})
 	-- Just in case you need to go to the very beginning of a line
-	map("n", "^", "0", {noremap = true})
+	vim.api.nvim_set_keymap("n", "^", "0", {noremap = true})
 end
