0edf86a0294f672acaa93b3529f5c35052b90fbd
Thomas Versteeg <t@versteeg.email>
2023-07-12 14:42:17 +0200

    feat(huion): check laptop with i3

--- huion.sh
+++ huion.sh
@@ -5,35 +5,54 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
-SWAYSOCK="/run/user/$(id -u thomas)/sway-ipc.$(id -u thomas).$(pgrep -x sway).sock"
+if [[ $HOSTNAME -eq 'cems' ]]; then
+	OUTPUT="HDMI-1"
+	TABLET="HUION Huion Tablet_GS1331 Pad pad"
+	TOUCH="HUION Huion Tablet_GS1331 Touch Strip pad"
+	PEN="HUION Huion Tablet_GS1331 Pen stylus"
 
-TABLET=$(swaymsg -s $SWAYSOCK -t get_outputs -r | jq '.[]|select(.model | startswith("Kamvas"))|.name' -r)
+	xsetwacom set "$PEN" MapToOutput "$OUTPUT"
+	xsetwacom set "$TABLET" MapToOutput "$OUTPUT"
 
-case $1 in
-	start)
-    swaymsg -s $SWAYSOCK exec "notify-send 'Tablet $TABLET connected'"
+	xsetwacom set "$TABLET" button "1" key "+Control_L +z"
+	xsetwacom set "$TABLET" button "2" key "+Control_L +Shift +z"
+	xsetwacom set "$TABLET" button "3" key "m"
+	xsetwacom set "$TABLET" button "8" key "+"
+	xsetwacom set "$TABLET" button "9" key "-"
+	xsetwacom set "$TABLET" button "10" key "Control_L"
+	xsetwacom set "$TABLET" button "11" key "Alt_L"
+	xsetwacom set "$TABLET" button "12" key "Shift_L"
+else
+	SWAYSOCK="/run/user/$(id -u thomas)/sway-ipc.$(id -u thomas).$(pgrep -x sway).sock"
 
-    sleep 3
+	TABLET=$(swaymsg -s $SWAYSOCK -t get_outputs -r | jq '.[]|select(.model | startswith("Kamvas"))|.name' -r)
 
-		swaymsg -s $SWAYSOCK workspace "tablet" output "$TABLET"
-		swaymsg -s $SWAYSOCK output "$TABLET" enable
-		swaymsg -s $SWAYSOCK output "$TABLET" scale 1 pos 0 2160
-		swaymsg -s $SWAYSOCK input type:tablet_pad map_to_output "$TABLET"
-		swaymsg -s $SWAYSOCK input type:tablet_tool map_to_output "$TABLET"
+	case $1 in
+		start)
+	    swaymsg -s $SWAYSOCK exec "notify-send 'Tablet $TABLET connected'"
 
-    input-remapper-control --command autoload
+	    sleep 3
 
-		swaymsg -s $SWAYSOCK 'assign [class="Aseprite"] workspace tablet'
-    swaymsg -s $SWAYSOCK exec "/home/thomas/.steam/steam/steamapps/common/Aseprite/aseprite"
-		;;
-	stop)
-    swaymsg -s $SWAYSOCK exec "notify-send 'Tablet $TABLET disconnected'"
+			swaymsg -s $SWAYSOCK workspace "tablet" output "$TABLET"
+			swaymsg -s $SWAYSOCK output "$TABLET" enable
+			swaymsg -s $SWAYSOCK output "$TABLET" scale 1 pos 0 2160
+			swaymsg -s $SWAYSOCK input type:tablet_pad map_to_output "$TABLET"
+			swaymsg -s $SWAYSOCK input type:tablet_tool map_to_output "$TABLET"
 
-    sleep 3
+	    input-remapper-control --command autoload
 
-		swaymsg -s $SWAYSOCK output "$TABLET" disable
-		;;
-	*)
-		echo 'Expected "start" or "stop"' >&2
-		exit 1
-esac
+			swaymsg -s $SWAYSOCK 'assign [class="Aseprite"] workspace tablet'
+	    swaymsg -s $SWAYSOCK exec "/home/thomas/.steam/steam/steamapps/common/Aseprite/aseprite"
+			;;
+		stop)
+	    swaymsg -s $SWAYSOCK exec "notify-send 'Tablet $TABLET disconnected'"
+
+	    sleep 3
+
+			swaymsg -s $SWAYSOCK output "$TABLET" disable
+			;;
+		*)
+			echo 'Expected "start" or "stop"' >&2
+			exit 1
+	esac
+fi
