6282fd2ac438a692e88c030355626df7524cc486
Thomas Versteeg <t@versteeg.email>
2023-07-26 09:50:03 +0200

    fix(helix): don't close parentheses automatically

--- huion.sh
+++ huion.sh
@@ -36,18 +36,21 @@ else
 			swaymsg -s $SWAYSOCK workspace "tablet" output "$TABLET"
 			swaymsg -s $SWAYSOCK output "$TABLET" enable
 			swaymsg -s $SWAYSOCK output "$TABLET" scale 1 pos 0 2160
+
+	    sleep 3
+
 			swaymsg -s $SWAYSOCK input type:tablet_pad map_to_output "$TABLET"
 			swaymsg -s $SWAYSOCK input type:tablet_tool map_to_output "$TABLET"
 
 	    input-remapper-control --command autoload
+
+			sleep 3
 
 			swaymsg -s $SWAYSOCK 'assign [class="Aseprite"] workspace tablet'
 	    swaymsg -s $SWAYSOCK exec "/home/thomas/.steam/steam/steamapps/common/Aseprite/aseprite"
 			;;
 		stop)
 	    swaymsg -s $SWAYSOCK exec "notify-send 'Tablet $TABLET disconnected'"
-
-	    sleep 3
 
 			swaymsg -s $SWAYSOCK output "$TABLET" disable
 			;;
