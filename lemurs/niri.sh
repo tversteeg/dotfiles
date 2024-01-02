dd7f79eec1b7d8902c0308edd45133435b3d705e
Thomas Versteeg <t@versteeg.email>
2024-01-02 22:18:23 +0100

    fix(sway): make sway work with lemurs

--- lemurs/niri.sh
+++ lemurs/niri.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
 export MOZ_ENABLE_WAYLAND=1
-export XDG_CURRENT_DESKTOP=niri
-export XDG_SESSION_TYPE=wayland
 
-# Import login manager env
-systemctl --user import-environment
-
-# Update env vars for D-Bus
-if hash dbus-update-activation-environment 2>/dev/null; then
-    dbus-update-activation-environment --all
-fi
-
-exec dbus-launch --sh-syntax --exit-with-session niri
+exec systemd-cat niri
--- lemurs/sway.sh
+++ lemurs/sway.sh
@@ -4,7 +4,5 @@
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 export MOZ_ENABLE_WAYLAND=1
-export XDG_CURRENT_DESKTOP=sway
-export XDG_SESSION_TYPE=wayland
 
-exec dbus-launch --sh-syntax --exit-with-session sway
+exec sway
