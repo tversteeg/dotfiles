40480ac14105407e25602db61037253a9a3e1e2c
Thomas Versteeg <t@versteeg.email>
2024-01-29 03:18:18 +0700

    chore(cleanup): remove unused files

--- forward-localhost-port.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp --dport $1 -j DNAT --to-destination $2
-sudo iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE
-sudo sysctl -w net.ipv4.conf.all.route_localnet=1
-
