4efe903e7889ba927a42d18a4b45c7736f1672c9
Thomas Versteeg <t@versteeg.email>
2024-04-08 10:11:40 +0200

    fix(lemurs): start again

--- lemurs/wayland/niri.sh
+++ lemurs/wayland/niri.sh
@@ -10,6 +10,9 @@ export XDG_SESSION_TYPE=wayland
 # Use wayland for floorp
 export MOZ_ENABLE_WAYLAND=1
 
+# Use wayland for QT
+QT_QPA_PLATFORM=wayland
+
 # Expose the local user SSH agent which is run as a systemd service, can be enabled on new configs with `servicectl --user enable --now ssh-agent`
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 
