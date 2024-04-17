b0bfe768187854c02fa8e660369ab11e3b974246
Thomas Versteeg <t@versteeg.email>
2024-04-17 09:19:20 +0200

    chore(waybar): move todo.txt to Sync

--- waybar-todo.sh
+++ waybar-todo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COUNT=$(wc -l < ~/todo.txt)
-TODOS=$(cat ~/todo.txt | head -c -1 - | sed -z 's/\n/\\n/g')
+COUNT=$(wc -l < ~/Sync/todo.txt)
+TODOS=$(cat ~/Sync/todo.txt | head -c -1 - | sed -z 's/\n/\\n/g')
 
 printf '{"text": "%s", "tooltip": "%s"}\n' "$COUNT" "$TODOS"
